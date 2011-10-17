9292aeaf7131a5e94d5bee220bd86fb7d8589ccc
Neil MacMunn <neil@gumstix.com>
2011-10-16 20:16:24 -0700

    lpc17xx_nvic.py, lpc17xx_pinsel.py: improved documentation.
    Signed-off-by: Neil MacMunn <neil@gumstix.com>

--- robovero/lpc17xx_nvic.py
+++ robovero/lpc17xx_nvic.py
@@ -1,5 +1,6 @@
-"""Nested vector interrupt controller client library functions. See LPC17xx 
-CMSIS-Compliant Standard Peripheral Firmware Driver Library documentation.
+"""Nested vector interrupt controller client library functions. Find 
+implementation details in LPC17xx CMSIS-Compliant Standard Peripheral Firmware 
+Driver Library documentation.
 """
 
 from internals import robocaller, cstruct
@@ -13,11 +14,38 @@ __license__ =     "BSD 2-Clause"
 __version__ =     "0.1"
 
 def NVIC_SetVTOR(offset):
-	return robocaller("NVIC_SetVTOR", "void", offset)
+  '''Set Vector Table Offset value.
+  
+  offset: Offset value
+  
+  '''
+  return robocaller("NVIC_SetVTOR", "void", offset)
 
 def NVIC_SCBDeInit():
-	return robocaller("NVIC_SCBDeInit", "void", )
+  '''De-initializes the SCB peripheral registers to their default reset values.
+  
+  These following SCB NVIC peripheral registers will be de-initialized:
+  - Interrupt Control State register
+  - Interrupt Vector Table Offset register
+  - Application Interrupt/Reset Control register
+  - System Control register
+  - Configuration Control register
+  - System Handlers Priority Registers
+  - System Handler Control and State Register
+  - Configurable Fault Status Register
+  - Hard Fault Status Register
+  - Debug Fault Status Register
+  
+  '''
+  return robocaller("NVIC_SCBDeInit", "void")
 
 def NVIC_DeInit():
-	return robocaller("NVIC_DeInit", "void", )
-
+  '''De-initializes the NVIC peripheral registers to their default reset values.
+  
+  These following NVIC peripheral registers will be de-initialized:
+  - Disable Interrupt (32 IRQ interrupt sources that matched with LPC17xx)
+  - Clear all Pending Interrupts (32 IRQ interrupt source that matched with LPC17xx)
+  - Clear all Interrupt Priorities (32 IRQ interrupt source that matched with LPC17xx)
+  
+  '''
+  return robocaller("NVIC_DeInit", "void")
--- robovero/lpc17xx_pinsel.py
+++ robovero/lpc17xx_pinsel.py
@@ -1,9 +1,8 @@
-"""Pin select client library functions. See LPC17xx CMSIS-Compliant
-Standard Peripheral Firmware Driver Library documentation.
+"""Pin select client library functions. Find implementation details in  LPC17xx
+CMSIS-Compliant Standard Peripheral Firmware Driver Library documentation.
 """
 
 from internals import robocaller, cstruct
-from lpc_types import _BIT
 
 __author__ =      "Neil MacMunn"
 __credits__ =     ["Neil MacMunn", "NXP MCU SW Application Team"]
@@ -13,68 +12,162 @@ __copyright__ =   "Copyright 2011, Gumstix Inc"
 __license__ =     "BSD 2-Clause"
 __version__ =     "0.1"
 
-PINSEL_PORT_0 = ((0))	
-PINSEL_PORT_1 = ((1))	
-PINSEL_PORT_2 = ((2))	
-PINSEL_PORT_3 = ((3))	
-PINSEL_PORT_4 = ((4))	
-PINSEL_FUNC_0 = ((0))	
-PINSEL_FUNC_1 = ((1))	
-PINSEL_FUNC_2 = ((2))	
-PINSEL_FUNC_3 = ((3))	
-PINSEL_PIN_0 = ((0)) 	
-PINSEL_PIN_1 = ((1)) 	
-PINSEL_PIN_2 = ((2)) 	
-PINSEL_PIN_3 = ((3)) 	
-PINSEL_PIN_4 = ((4)) 	
-PINSEL_PIN_5 = ((5)) 	
-PINSEL_PIN_6 = ((6)) 	
-PINSEL_PIN_7 = ((7)) 	
-PINSEL_PIN_8 = ((8)) 	
-PINSEL_PIN_9 = ((9)) 	
-PINSEL_PIN_10 = ((10)) 	
-PINSEL_PIN_11 = ((11)) 	
-PINSEL_PIN_12 = ((12)) 	
-PINSEL_PIN_13 = ((13)) 	
-PINSEL_PIN_14 = ((14)) 	
-PINSEL_PIN_15 = ((15)) 	
-PINSEL_PIN_16 = ((16)) 	
-PINSEL_PIN_17 = ((17)) 	
-PINSEL_PIN_18 = ((18)) 	
-PINSEL_PIN_19 = ((19)) 	
-PINSEL_PIN_20 = ((20)) 	
-PINSEL_PIN_21 = ((21)) 	
-PINSEL_PIN_22 = ((22)) 	
-PINSEL_PIN_23 = ((23)) 	
-PINSEL_PIN_24 = ((24)) 	
-PINSEL_PIN_25 = ((25)) 	
-PINSEL_PIN_26 = ((26)) 	
-PINSEL_PIN_27 = ((27)) 	
-PINSEL_PIN_28 = ((28)) 	
-PINSEL_PIN_29 = ((29)) 	
-PINSEL_PIN_30 = ((30)) 	
-PINSEL_PIN_31 = ((31)) 	
-PINSEL_PINMODE_PULLUP = ((0))	
-PINSEL_PINMODE_TRISTATE = ((2))	
-PINSEL_PINMODE_PULLDOWN = ((3)) 	
-PINSEL_PINMODE_NORMAL = ((0))	
-PINSEL_PINMODE_OPENDRAIN = ((1)) 	
-PINSEL_I2C_Normal_Mode = ((0))	
-PINSEL_I2C_Fast_Mode = ((1)) 	
-PINSEL_I2CPADCFG_SDADRV0 = _BIT(0) 
-PINSEL_I2CPADCFG_SDAI2C0 = _BIT(1) 
-PINSEL_I2CPADCFG_SCLDRV0 = _BIT(2) 
-PINSEL_I2CPADCFG_SCLI2C0 = _BIT(3) 
+# Macros define for PORT Selection
+# PORT 0
+PINSEL_PORT_0 = ((0))  
+# PORT 1
+PINSEL_PORT_1 = ((1))  
+# PORT 2
+PINSEL_PORT_2 = ((2))  
+# PORT 3
+PINSEL_PORT_3 = ((3))  
+# PORT 4
+PINSEL_PORT_4 = ((4))
+
+# Macros define for Pin Function selection
+# default function
+PINSEL_FUNC_0 = ((0))  
+# first alternate function
+PINSEL_FUNC_1 = ((1))  
+# second alternate function
+PINSEL_FUNC_2 = ((2))  
+# third or reserved alternate function
+PINSEL_FUNC_3 = ((3))  
+
+# Macros define for Pin Number of Port
+# Pin 0
+PINSEL_PIN_0 = ((0))   
+# Pin 1
+PINSEL_PIN_1 = ((1))   
+# Pin 2
+PINSEL_PIN_2 = ((2))   
+# Pin 3
+PINSEL_PIN_3 = ((3))   
+# Pin 4
+PINSEL_PIN_4 = ((4))   
+# Pin 5
+PINSEL_PIN_5 = ((5))   
+# Pin 6
+PINSEL_PIN_6 = ((6))   
+# Pin 7
+PINSEL_PIN_7 = ((7))   
+# Pin 8
+PINSEL_PIN_8 = ((8))   
+# Pin 9
+PINSEL_PIN_9 = ((9))   
+# Pin 10
+PINSEL_PIN_10 = ((10))   
+# Pin 11
+PINSEL_PIN_11 = ((11))   
+# Pin 12
+PINSEL_PIN_12 = ((12))   
+# Pin 13
+PINSEL_PIN_13 = ((13))   
+# Pin 14
+PINSEL_PIN_14 = ((14))   
+# Pin 15
+PINSEL_PIN_15 = ((15))   
+# Pin 16
+PINSEL_PIN_16 = ((16))   
+# Pin 17
+PINSEL_PIN_17 = ((17))   
+# Pin 18
+PINSEL_PIN_18 = ((18))   
+# Pin 19
+PINSEL_PIN_19 = ((19))   
+# Pin 20
+PINSEL_PIN_20 = ((20))   
+# Pin 21
+PINSEL_PIN_21 = ((21))   
+# Pin 22
+PINSEL_PIN_22 = ((22))   
+# Pin 23
+PINSEL_PIN_23 = ((23))   
+# Pin 24
+PINSEL_PIN_24 = ((24))   
+# Pin 25
+PINSEL_PIN_25 = ((25))   
+# Pin 26
+PINSEL_PIN_26 = ((26))   
+# Pin 27
+PINSEL_PIN_27 = ((27))   
+# Pin 28
+PINSEL_PIN_28 = ((28))   
+# Pin 29
+PINSEL_PIN_29 = ((29))   
+# Pin 30
+PINSEL_PIN_30 = ((30))   
+# Pin 31
+PINSEL_PIN_31 = ((31)) 
+
+# Macros define for Pin mode
+# PINSEL_PINMODE_PULLUP
+PINSEL_PINMODE_PULLUP = ((0))  
+# Tri-state
+PINSEL_PINMODE_TRISTATE = ((2))  
+# Internal pull-down resistor
+PINSEL_PINMODE_PULLDOWN = ((3))   
+# Pin is in the normal (not open drain) mode
+PINSEL_PINMODE_NORMAL = ((0))  
+# Pin is in the open drain mode
+PINSEL_PINMODE_OPENDRAIN = ((1))
+
+# Macros define for I2C mode
+# The standard drive mode
+PINSEL_I2C_Normal_Mode = ((0))  
+# Fast Mode Plus drive mode
+PINSEL_I2C_Fast_Mode = ((1))   
 
 class PINSEL_CFG_Type(cstruct):
-	pass
+  '''Pin configuration structure.
+  
+  Portnum:  Port Number, should be PINSEL_PORT_x, where x should be in range
+            from 0 to 4
+  Pinnum: Pin Number, should be PINSEL_PIN_x, where x should be in range from 0
+          to 31
+  Funcnum:  Function Number, should be PINSEL_FUNC_x, where x should be in range
+            from 0 to 3
+  Pinmode:  Pin Mode, should be:
+            PINSEL_PINMODE_PULLUP: Internal pull-up resistor
+						PINSEL_PINMODE_TRISTATE: Tri-state
+						PINSEL_PINMODE_PULLDOWN: Internal pull-down resistor
+  OpenDrain:  OpenDrain mode, should be:
+              PINSEL_PINMODE_NORMAL: Pin is in the normal (not open drain) mode
+              PINSEL_PINMODE_OPENDRAIN: Pin is in the open drain mode  
+  ptr:  LPC1769 memory address where structure is stored. Use this in place of
+        the C reference operator (&).
+          
+  '''
+  pass
 
 def PINSEL_SetI2C0Pins(i2cPinMode, filterSlewRateEnable):
-	return robocaller("PINSEL_SetI2C0Pins", "void", i2cPinMode, filterSlewRateEnable)
+  '''Setup I2C0 pins.
+  
+  i2cPinMode: I2C pin mode, should be one of the following:
+              PINSEL_I2C_Normal_Mode : The standard drive mode
+              PINSEL_I2C_Fast_Mode : Fast Mode Plus drive mode
+  filterSlewRateEnable: should be:
+                        ENABLE: Enable filter and slew rate.
+                        DISABLE: Disable filter and slew rate.
+    
+  '''
+  return robocaller("PINSEL_SetI2C0Pins", "void", i2cPinMode, filterSlewRateEnable)
 
 def PINSEL_ConfigPin(PinCfg):
-	return robocaller("PINSEL_ConfigPin", "void", PinCfg)
+  '''Configure Pin corresponding to specified parameters passed in the PinCfg.
+  
+  PinCfg: Pointer to a PINSEL_CFG_Type structure that contains the configuration
+          information for the specified pin.
+  
+  '''
+  return robocaller("PINSEL_ConfigPin", "void", PinCfg)
 
 def PINSEL_ConfigTraceFunc(NewState):
-	return robocaller("PINSEL_ConfigTraceFunc", "void", NewState)
-
+  '''Configure trace function.
+  
+  NewState: should be one of the following:
+            ENABLE : Enable Trace Function
+            DISABLE : Disable Trace Function
+  
+  '''
+  return robocaller("PINSEL_ConfigTraceFunc", "void", NewState)
